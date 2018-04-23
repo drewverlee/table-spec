6b8480bf8b7096e8bd78d653f8b6cc845ef593dc
drew verlee <drew.verlee@gmail.com>
2018-04-23 15:01:28 -0400

    comment things out

--- test/table_spec/dev.clj
+++ test/table_spec/dev.clj
@@ -5,7 +5,7 @@
 
 
 
-(s/exercise :table/customers)
+;(s/exercise :table/customers)
 
 ;([#:customers{:mfa false,
 ;              :key #uuid"22c07e92-93a1-4367-afbf-9ce9dadb30fe",
@@ -33,7 +33,7 @@
 ;              :created_at #inst"1969-12-31T23:59:59.999000000-00:00"}])
 
 
-(s/exercise :table/nodes)
+;(s/exercise :table/nodes)
 
 ;=>
 ;([#:nodes{:updated_at #inst"1970-01-01T00:00:00.000000000-00:00",
@@ -46,15 +46,15 @@
 ;          :customer_uuid #uuid"0b3efaf2-da7b-4d1a-a7ef-b643bd18c0af",
 ;          :uuid #uuid"f553a992-f65a-4d2f-a32c-513caee5582a",
 ;          :created_at #inst"1969-12-31T23:59:59.999000000-00:00",
-u
+
 ;          :display_name "",
 ;          :customer_id -1}])
 
-(-> {:connection-uri "jdbc:postgresql://localhost:5439/resolute_cloud_dev?user=postgres" :schema "public"}
-    (t/tables)
-    (t/register))
+; (-> {:connection-uri "jdbc:postgresql://localhost:5439/resolute_cloud_dev?user=postgres" :schema "public"}
+;     (t/tables)
+;     (t/register))
 
-(s/exercise :table/point_types)
+;(s/exercise :table/point_types)
 
 ;=>
 ;([#:point_types{:name ""} #:point_types{:name ""}
@@ -71,9 +71,9 @@ u
 ; point types isn't what we need
 
 
-(s/def ::table/point_types (s/keys))
+; (s/def ::table/point_types (s/keys))
 
-(s/exercise :table/nodes)
+; (s/exercise :table/nodes)
 
-(s/exercise :table/points)
+; (s/exercise :table/points)
 
